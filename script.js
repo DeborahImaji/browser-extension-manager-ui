9f3542e37752fa0c77ca6f30460f337df3adc91f
Deborah Imaji <101178292+DeborahImaji@users.noreply.github.com>
2025-03-27 19:56:32 +0100

    Began coding the header

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function changeTheme() {
-    let body = document.querySelector("#body");
+    console.log("Theme button clicked");
+    let body = document.querySelector("body");
+    console.log(body);
+
+    if (body.classList.contains("dark")) {
+        body.classList.remove("dark");
+        // It's currently dark, switching to light
+    } else {
+        body.classList.add("dark");
+        // It's currently light, switching to dark
+    }
 }
 
 let themeButton = document.querySelector("#theme-button");
+console.log(themeButton);
 themeButton.addEventListener('click', changeTheme);
